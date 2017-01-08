c8f3defa47b6f62a636bb2369bb54af1126a7aa1
Joe Khoury <joe.el.k97@gmail.com>
2017-01-07 20:01:57 -0500

    Tentative new_vector function.

--- vector/vector.c
+++ vector/vector.c
@@ -1,5 +1,16 @@
 #include "vector.h"
 
-vector* new_vector () {}
+vector* new_vector () {
+    vector* ret = (vector*)(malloc(sizeof(vector)));
+
+    // We can assume no memory errors occur.
+
+    ret->data = (void**)(malloc(sizeof(void*) * 8));
+    // Size is the number of elements, capacity is how much the vector can hold.
+    ret->size = 0;
+    ret->capacity = 8;
+
+    return ret;
+}
 
 void delete_vector () {}
