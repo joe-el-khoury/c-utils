ef6c4d00802e40e04f107353aac8ec371dee5cba
Joe Khoury <joe.el.k97@gmail.com>
2017-01-07 20:03:55 -0500

    Removed magic number.

--- vector/vector.c
+++ vector/vector.c
@@ -1,14 +1,16 @@
 #include "vector.h"
 
+#define INITIAL_ALLOC 8
+
 vector* new_vector () {
     vector* ret = (vector*)(malloc(sizeof(vector)));
 
     // We can assume no memory errors occur.
 
-    ret->data = (void**)(malloc(sizeof(void*) * 8));
+    ret->data = (void**)(malloc(sizeof(void*) * INITIAL_ALLOC));
     // Size is the number of elements, capacity is how much the vector can hold.
     ret->size = 0;
-    ret->capacity = 8;
+    ret->capacity = INITIAL_ALLOC;
 
     return ret;
 }
