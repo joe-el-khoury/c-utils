50812327e9fdadc7f1eff57613d6a064f155b8c3
Joe Khoury <joe.el.k97@gmail.com>
2017-01-09 15:49:31 -0500

    Wrote entry_pair struct.

--- hashtable/hashtable.h
+++ hashtable/hashtable.h
@@ -1,4 +1,29 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+// Keys and values can only be ints and strs for now.
+struct entry_pair {
+    union {
+        struct {
+            int key;
+            int val;
+        } _int_int;
+
+        struct {
+            int   key;
+            char* val;
+        } _int_str;
+
+        struct {
+            char* key;
+            char* val;
+        } _str_str;
+
+        struct {
+            char* key;
+            int   val;
+        } _str_int;
+    };
+};
+
 #endif /* HASHTABLE_H */
