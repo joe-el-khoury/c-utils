bf58df783863a512f21a3835d62d5299d1f7683d
Joe Khoury <joe.el.k97@gmail.com>
2017-01-11 21:32:07 -0500

    Wrote function to create a str-int pair.

--- hashtable/hashtable.c
+++ hashtable/hashtable.c
@@ -1,17 +1,5 @@
 #include "hashtable.h"
 
-// Allocates a pair.
-entry_pair* _new_pair () {
-    return (entry_pair*)(malloc(sizeof(entry_pair)));
-}
-
-entry_pair* _new_int_int_pair (int _key, int _val) {
-    entry_pair* ep = _new_pair();
-    ep->_int_int.key = _key;
-    ep->_int_int.val = _val;
-    return ep;
-}
-
 char* _dup_str (char* _str) {
     if (!_str) {
         return NULL;
@@ -25,6 +13,18 @@ char* _dup_str (char* _str) {
     return ret;
 }
 
+// Allocates a pair.
+entry_pair* _new_pair () {
+    return (entry_pair*)(malloc(sizeof(entry_pair)));
+}
+
+entry_pair* _new_int_int_pair (int _key, int _val) {
+    entry_pair* ep = _new_pair();
+    ep->_int_int.key = _key;
+    ep->_int_int.val = _val;
+    return ep;
+}
+
 entry_pair* _new_int_str_pair (int _key, char* _val) {
     entry_pair* ep = _new_pair();
     ep->_int_str.key = _key;
@@ -32,6 +32,16 @@ entry_pair* _new_int_str_pair (int _key, char* _val) {
     return ep;
 }
 
-entry_pair* _new_str_str_pair (char* _key, char* _val) {}
+entry_pair* _new_str_str_pair (char* _key, char* _val) {
+    entry_pair* ep = _new_pair();
+    ep->_str_str.key = _dup_str(_key);
+    ep->_str_str.val = _dup_str(_val);
+    return ep;
+}
 
-entry_pair* _new_str_int_pair (char* _key, int _val) {}
+entry_pair* _new_str_int_pair (char* _key, int _val) {
+    entry_pair* ep = _new_pair();
+    ep->_str_int.key = _dup_str(_key);
+    ep->_str_int.val = _int;
+    return ep;
+}
