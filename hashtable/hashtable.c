e953fdd25a86d58a35f185b20be8b7ff16715630
Joe Khoury <joe.el.k97@gmail.com>
2017-01-11 21:29:30 -0500

    Wrote function to create an int-str pair.

--- hashtable/hashtable.c
+++ hashtable/hashtable.c
@@ -12,7 +12,7 @@ entry_pair* _new_int_int_pair (int _key, int _val) {
     return ep;
 }
 
-char* dup_str (char* _str) {
+char* _dup_str (char* _str) {
     if (!_str) {
         return NULL;
     }
@@ -25,7 +25,12 @@ char* dup_str (char* _str) {
     return ret;
 }
 
-entry_pair* _new_int_str_pair (int _key, char* _val) {}
+entry_pair* _new_int_str_pair (int _key, char* _val) {
+    entry_pair* ep = _new_pair();
+    ep->_int_str.key = _key;
+    ep->_int_str.val = _dup_str(_val);
+    return ep;
+}
 
 entry_pair* _new_str_str_pair (char* _key, char* _val) {}
 
